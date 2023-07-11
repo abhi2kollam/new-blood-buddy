410b8a36387462016787f74277a0886c09296bd1
Abhilash Pillai <abhi2kollam@gmail.com>
2023-07-11 11:51:25 +0530

    Initial commit

---
+++ apps/blood-buddy/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'blood-buddy',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/blood-buddy',
+};
